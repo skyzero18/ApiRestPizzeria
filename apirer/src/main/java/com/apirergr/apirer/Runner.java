1d5c8e40103b33a04853c0336fc4c974919f7b7d
skyzero18 <lucianojofre122@gmail.com>
2024-09-25 15:26:13 -0300

    Final 1.0

--- apirer/src/main/java/com/apirergr/apirer/Runner.java
+++ apirer/src/main/java/com/apirergr/apirer/Runner.java
@@ -1,8 +1,12 @@
-package com.apirergr.apirer.repositorios;
+package com.apirergr.apirer;
 import com.apirergr.apirer.modelos.Clientes;
 import com.apirergr.apirer.modelos.Direccion;
 import com.apirergr.apirer.modelos.Pizzeria;
 import com.apirergr.apirer.modelos.Vendedores;
+import com.apirergr.apirer.repositorios.ClienteRepo;
+import com.apirergr.apirer.repositorios.DireccionRepo;
+import com.apirergr.apirer.repositorios.PizzeriaRepo;
+import com.apirergr.apirer.repositorios.VendedoresRepo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import java.util.Set;
